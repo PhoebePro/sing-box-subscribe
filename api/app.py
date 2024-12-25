965d224c449bdbfbcefa5e989c50cb64a7944f7d
Pearce <44561721+PhoebePro@users.noreply.github.com>
2024-12-25 15:04:42 +0800

    Update app.py

--- api/app.py
+++ api/app.py
@@ -309,9 +309,6 @@ def generate_config():
         # add file download
         return send_file(config_file_path, as_attachment=True, download_name=CONFIG_FILE_NAME )
 
-        response = send_file(config_file_path, as_attachment=True, download_name=CONFIG_FILE_NAME)
-        os.remove(config_file_path)  # 下载后删除
-        return response
         
         return Response(config_content, content_type='text/plain; charset=utf-8')
     except subprocess.CalledProcessError as e:
